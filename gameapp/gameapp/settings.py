902d62237fe8c4ebcf0596bf2b45984524992f22
Hrach Ghazaryan <hrach.ghazaryan.web@gmail.com>
2021-06-26 22:41:21 +0400

    New updates for Heroku deployment

--- gameapp/gameapp/settings.py
+++ gameapp/gameapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4hn$-*w0bq92o+fgunst*2g6o$&o5fr*^rlz-gsy!d)fd49p%$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
